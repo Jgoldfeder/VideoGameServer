693c4fb271b5ad4ff88e1b7f2c77cee6c3fbbb82
jgoldfeder <jgoldfed@mail.yu.edu>
2020-04-13 13:39:26 -0400

    fixed bug

--- Main.java
+++ Main.java
@@ -29,10 +29,10 @@ public class Main extends Application
     public static void main(String[] args) {
         playerNum = Integer.parseInt(args[0]);
         if(args.length>1){
-            ip_name = args[0];
+            ip_name = args[1];
         }
         if(args.length>2){
-            port = Integer.parseInt(args[1]);
+            port = Integer.parseInt(args[2]);
         }
  
         launch(args);
